13211d77b61dc6bb3a6bb457304cac90e21a6ef1
simon.abt <simon.abt1401@gmail.com>
2022-06-14 16:23:05 +0200

    Unit Tests

--- webapp/test/unit/unitTests.qunit.js
+++ webapp/test/unit/unitTests.qunit.js
@@ -4,7 +4,7 @@ QUnit.config.autostart = false;
 sap.ui.getCore().attachInit(function () {
   "use strict";
 
-  sap.ui.require(["com.sbt.IHKProject/abschlussproject/webapp/test/unit/allTests"], function () {
+  sap.ui.require(["com/sbt/IHKProject/test/unit/allTests"], function () {
     QUnit.start();
   });
 });
